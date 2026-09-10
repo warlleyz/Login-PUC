226ee8deab1d47b58170e9c30f5e511a27e8ac48
Warlley Silva <warlleysilvax@gmail.com>
2026-09-10 18:25:41 -0300

    feat: cria entidade usuario

---
+++ src/main/java/com/pucminas/loginpuc/model/Usuario.java
@@ -0,0 +1,69 @@
+package com.pucminas.loginpuc.model;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+
+@Entity
+@Table(name = "usuarios")
+public class Usuario {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    private String nome;
+    private String username;
+    private String email;
+    private String senha;
+    public Usuario() {
+    }
+
+    public Usuario(String nome, String username, String email, String senha) {
+        this.nome = nome;
+        this.username = username;
+        this.email = email;
+        this.senha = senha;
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getNome() {
+        return nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    public String getUsername() {
+        return username;
+    }
+
+    public void setUsername(String username) {
+        this.username = username;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getSenha() {
+        return senha;
+    }
+
+    public void setSenha(String senha) {
+        this.senha = senha;
+    }
+}
