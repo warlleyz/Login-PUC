aefcb8cb65b4cb6d326a2ac3b55c6b83835e711d
Warlley Silva <warlleysilvax@gmail.com>
2026-09-10 18:04:49 -0300

    chore: cria estrutura inicial do projeto Spring Boot

---
+++ src/main/java/com/pucminas/loginpuc/LoginpucApplication.java
@@ -0,0 +1,13 @@
+package com.pucminas.loginpuc;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LoginpucApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LoginpucApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/pucminas/loginpuc/LoginpucApplicationTests.java
@@ -0,0 +1,13 @@
+package com.pucminas.loginpuc;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LoginpucApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
